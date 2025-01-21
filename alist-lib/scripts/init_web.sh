797718b7cbb38b51c4577b9436025edde6ebe81f
Clouddark75 <84534855+Clouddark75@users.noreply.github.com>
2025-01-21 09:52:36 -0400

    Update init_web.sh

--- alist-lib/scripts/init_web.sh
+++ alist-lib/scripts/init_web.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl -L https://yugenmangas7-work.hf.space/d/Local/home/user/dist.tar -o dev.tar
-tar -zxvf dev.tar
+curl -L https://yugenmangas7-work.hf.space/d/Local/home/user/dist.tar -o dev.tar.gz
+tar -zxvf dev.tar.gz
 rm -rf ../public/dist
 mv -f dist ../public
-rm -rf dev.tar
+rm -rf dev.tar.gz
