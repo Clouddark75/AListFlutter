e09bc1a78ae2e599f037980dc4a53acc061744f7
Clouddark75 <84534855+Clouddark75@users.noreply.github.com>
2025-01-21 09:41:35 -0400

    Update init_web.sh

--- alist-lib/scripts/init_web.sh
+++ alist-lib/scripts/init_web.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl -L https://download948.mediafire.com/kqgaebk6z4vgWIvZsK01RxLjQY2-PTr84e0AJ68YBbmXwrosUbKhKWv4p4q-qdPRHL7kQTcaDwE2zghTlbjpXphaLGXerhTTaCglJqvvw0cyUgA7llER6a0ZxDgMe4XPtnomDynJ4bbWpjpz02N2xcl5Dkqcfqp3c4KoNLd5WRr-/jhkx8tko4iwchza/dev-dist.zip -o dev.zip
-tar -zxvf dev.zip
+curl -L https://yugenmangas7-work.hf.space/d/Local/home/user/dist.tar -o dev.tar
+tar -zxvf dev.tar
 rm -rf ../public/dist
 mv -f dist ../public
-rm -rf dev.zip
+rm -rf dev.tar
