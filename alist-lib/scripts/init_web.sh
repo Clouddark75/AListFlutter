4da788885ae21e23cbbcf0eb8d8dcd129f903806
Clouddark75 <84534855+Clouddark75@users.noreply.github.com>
2025-01-20 23:33:37 -0400

    Update init_web.sh

--- alist-lib/scripts/init_web.sh
+++ alist-lib/scripts/init_web.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl -L https://github.com/alist-org/web-dist/archive/refs/heads/dev.zip -o dev.zip
-tar -zxvf dev.zip
-rm -rf ../public/dist
-mv -f dist ../public
-rm -rf dev.zip
+curl -L https://github.com/alist-org/web-dist/archive/refs/heads/dev.zip -o web-dist-dev.zip
+zip -zxvf web-dist-dev.zip
+rm -rf ../public/web-dist-dev/dist
+mv -f web-dist-dev/dist ../public
+rm -rf web-dist-dev.zip
