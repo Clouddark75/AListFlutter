8c025aa5f16928f1d5d2081ffd12562825ce1454
Clouddark75 <84534855+Clouddark75@users.noreply.github.com>
2025-04-03 14:15:25 -0400

    Update init_web.sh

--- alist-lib/scripts/init_web.sh
+++ alist-lib/scripts/init_web.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -L https://files.catbox.moe/z59a55.gz -o dist.tar.gz
+curl -L https://files.catbox.moe/z8b5v7.gz -o dist.tar.gz
 tar -zxvf dist.tar.gz
 rm -rf ../public/dist
 mv -f dist ../public
