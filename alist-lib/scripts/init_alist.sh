8a73e74c1e2115be78e704e4d9b1fc5114819b5b
Clouddark75 <84534855+Clouddark75@users.noreply.github.com>
2025-05-22 16:07:26 -0400

    Update init_alist.sh

--- alist-lib/scripts/init_alist.sh
+++ alist-lib/scripts/init_alist.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-GIT_REPO="https://github.com/alist-org/alist.git"
+GIT_REPO="https://github.com/ykxVK8yL5L/alist.git"
 TAG_NAME=$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='version:refname' --tags $GIT_REPO | tail --lines=1 | cut --delimiter='/' --fields=3)
 
 echo "AList - ${TAG_NAME}"
 rm -rf ./src
 unset GIT_WORK_TREE
-git clone --branch "$TAG_NAME" https://github.com/alist-org/alist.git ./src
+git clone https://github.com/ykxVK8yL5L/alist.git ./src
 rm -rf ./src/.git
 
 mv -f ./src/* ../
 rm -rf ./src
-
-cd ../
-go mod edit -replace github.com/djherbis/times@v1.6.0=github.com/jing332/times@latest
