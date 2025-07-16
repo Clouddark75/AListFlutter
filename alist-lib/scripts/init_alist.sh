9e5795ae1771bcd9f2a1ce72d696b2f365fc3fbd
Clouddark75 <84534855+Clouddark75@users.noreply.github.com>
2025-07-16 13:55:19 -0400

    Update init_alist.sh

--- alist-lib/scripts/init_alist.sh
+++ alist-lib/scripts/init_alist.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-GIT_REPO="https://github.com/OpenListTeam/OpenList.git"
+GIT_REPO="https://github.com/Clouddark75/OpenList.git"
 TAG_NAME=$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='version:refname' --tags $GIT_REPO | tail --lines=1 | cut --delimiter='/' --fields=3)
 
 echo "OpenList - ${TAG_NAME}"
 rm -rf ./src
 unset GIT_WORK_TREE
-git clone --branch "$TAG_NAME" https://github.com/OpenListTeam/OpenList.git ./src
+git clone --branch "1.0.4-Fix" https://github.com/Clouddark75/OpenList.git ./src
 rm -rf ./src/.git
 
 mv -f ./src/* ../
