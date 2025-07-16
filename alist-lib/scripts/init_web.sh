5d850e15ac745fb4ee7fb2f509a17e48ec573e6c
Clouddark75 <84534855+Clouddark75@users.noreply.github.com>
2025-07-16 13:18:31 -0400

    Update init_web.sh

--- alist-lib/scripts/init_web.sh
+++ alist-lib/scripts/init_web.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -L https://github.com/OpenListTeam/OpenList-Frontend/releases/download/rolling/openlist-frontend-dist-v4.0.8-950c386.tar.gz -o dist.tar.gz
+curl -L https://files.catbox.moe/0ms8dq.gz -o dist.tar.gz
 tar -zxvf dist.tar.gz
 rm -rf ../public/dist
 mv -f dist ../public
